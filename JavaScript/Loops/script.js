693d2219ad39ee84304435520794c5bc788011cd
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-01-03 23:21:23 -0300

    commit_JS

---
+++ JavaScript/Loops/script.js
@@ -0,0 +1,35 @@
+// ex 1
+
+// let valor = 0
+
+// while (valor < 10){
+//     console.log(valor)
+//     valor++
+// }
+
+
+
+// ex 2
+
+// let valor = 0;
+
+// do {
+//     console.log(valor);
+//     valor++
+// } while (valor < 10)
+
+
+// ex 3
+
+// for(let valor = 0; valor < 10; valor++){
+//     console.log(valor);
+// }
+
+
+// ex 4
+
+let valores = [1,2,3,4,5]
+
+for(let valor of valores){
+    console.log(valor);
+}
---
+++ JavaScript/funções/script.js
@@ -0,0 +1,29 @@
+// function escrever(){
+//      console.log('Aqui esta a frase da função');
+// }
+
+// escrever()
+
+
+// function escrever(mensagem){
+//     console.log(mensagem);
+// }
+
+// escrever('Olá Mundo!')
+// escrever('Este é um exemplo de função')
+
+
+// function adicionar(a, b){
+//      return a + b
+// }
+
+// let resultado = adicionar(100,100)
+// console.log(`O resultado é ${resultado}`);
+
+
+// function saudacao(mensagem = 'Bom dia!'){
+//     console.log(mensagem);
+// }
+
+saudacao()
+saudacao('Boa tarde!!')
