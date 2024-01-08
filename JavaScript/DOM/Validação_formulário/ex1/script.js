7357e3e8390f02b4e12ba19dd6fb3ea11a185ebf
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-01-08 17:18:23 -0300

    Commit_form_validation

--- JavaScript/DOM/Validação_formulário/ex1/script.js
+++ JavaScript/DOM/Validação_formulário/ex1/script.js
@@ -1,6 +1,24 @@
-// Validação de formulários
+// Validação de formulários com Negação de click e alerta
 
-document.querySelector('form').addEventListener('submit', (event) => {
-    // Buscar valores dos campos input
-    let usuario = event.usuario.value
-} )
+    document.querySelector('.form_login').addEventListener('submit', (e) => {
+       //Buscar valores dos campos
+      let usuario = e.target.usuario.value
+      let senha = e.target.senha.value
+      let submit = true
+
+      // Validar usuario
+      if(usuario === ''){
+          submit = false
+      }
+
+      // Validar senha
+      if(senha === ''){
+          submit = false
+      }
+
+      // Verificar se o formulário pode ser submetido
+      if(!submit){
+          e.preventDefault()
+          window.alert('Favor preencher todos os dados')
+      }
+  })
--- JavaScript/DOM/geral/script.js
+++ JavaScript/DOM/geral/script.js
@@ -6,8 +6,15 @@
 
 // console.log(documento.lang); // mostra a definição de lang no site
 
-for(let a = 1; a < 10; a++) {
-    console.log(a);
-}
+// for(let a = 1; a < 10; a++) {
+//     console.log(a);
+// }
+
+document.querySelector('button').addEventListener('click', () => {
+    let imagem = document.createElement('img')
+    imagem.setAttribute('src', 'bem vindo mobile.png')
+    document.body.appendChild(imagem)
+
+} )
 
 
