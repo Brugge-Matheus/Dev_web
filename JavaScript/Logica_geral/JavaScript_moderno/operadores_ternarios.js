3b853ca08783afc184de878d70b5a2ddb3747862
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-01-17 23:15:25 -0300

    Commit_estudosJS

---
+++ JavaScript/Logica_geral/JavaScript_moderno/operadores_ternarios.js
@@ -0,0 +1,13 @@
+/* Os operadores ternários servem como um atalho para o if else comprimindo o código em uma linha
+ Sua estrutura é: 'condition ? expr1 : expr2' podendo tambem ser adicionada dentro de uma variavel sendo:
+ 'let variavel = condition ? expr1 : expr2'
+*/
+
+let nome = '3'
+nome == 'Matheus' ? console.log('Verdadeiro') : console.log('falso')
+
+//---------//
+
+let numero = 10
+let resultado = numero <= 10 ? 'True' : 'False'
+console.log(resultado);
