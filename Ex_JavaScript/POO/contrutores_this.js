5fdce9c277ea1be8d402b44c467eae98da6bf2a2
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-01-25 23:06:08 -0300

    Commit_estudos

--- Ex_JavaScript/POO/contrutores_this.js
+++ Ex_JavaScript/POO/contrutores_this.js
@@ -1,16 +1,35 @@
 // Programação orientada a objetos - Código programado com objetos que se comunicam entre sí
 
 class Person {
-    age = 1; // Cria uma variavel que sera usada para todas as pessoas
+    age = 0; // Cria uma variavel que sera usada para todas as pessoas
+    steaps = 0;
 
     constructor(name, age) {
         this.name = name;
         // this.age = age
     }
+
+    takeSteap() {
+        this.steaps++;
+    }
+
+    setAge(newAge) {
+        if(typeof newAge === 'number') {
+            this.age = newAge;
+
+        } else {
+            console.log('Error');
+        }    
+        
+    }
 }
+// const Person2 = new Person('Júlia', 7)
+// const Person3 = new Person('Kleber', 21)
 
 const Person1 = new Person('Matheus', 18)
-const Person2 = new Person('Júlia', 7)
-const Person3 = new Person('Kleber', 21)
+Person1.takeSteap();
 
-console.log(Person1.name);
+Person1.setAge(10)
+
+console.log(`Passos de ${Person1.name}: ${Person1.steaps}`);
+console.log(`A idade de ${Person1.name} é ${Person1.age}`);
