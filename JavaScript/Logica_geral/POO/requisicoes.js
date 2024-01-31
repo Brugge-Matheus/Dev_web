662b39e7a2373596a725fe2305690a101c2440c1
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-01-31 00:53:36 -0300

    commit_estudos

--- JavaScript/Logica_geral/POO/requisicoes.js
+++ JavaScript/Logica_geral/POO/requisicoes.js
@@ -2,8 +2,37 @@
 Oque é um Callback
 */
 
-function clickCallback() {
-    window.alert('Clicou no botão');
+// Promisse (Promessa)
+
+
+// Requisição simples
+
+const CallApi = () => {
+    let req = fetch('https://jsonplaceholder.typicode.com/postss')
+
+    req.then((response) => {
+        return response.json();
+
+
+    }).then((json) => {
+        window.alert(`Título do primeiro post: ${json[0].title}`)
+
+
+    }).catch((error) => {
+        console.log('Erro ao chamar a API');
+        console.log(error);
+
+
+    }).finally(() => {
+        window.alert('Finally process!!')
+    })
+
 }
 
-document.querySelector('button').addEventListener('click', clickCallback);
+
+
+function somar(x, y) {
+    return x + y;
+}
+
+document.querySelector('button').addEventListener('click', CallApi);
