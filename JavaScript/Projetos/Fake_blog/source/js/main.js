66ff4e2f10d8c8898a0c9a3e491fa11122f963f7
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-02-01 15:35:50 -0300

    commit_project

--- JavaScript/Projetos/Fake_blog/source/js/main.js
+++ JavaScript/Projetos/Fake_blog/source/js/main.js
@@ -1,54 +1,60 @@
-
-
 const readPosts = async () => {
-    const postArea = document.querySelector('.posts');
-    postArea.innerHTML = 'Carregando....';
+    let areaPost = document.querySelector('.posts');
+    areaPost.innerHTML = 'Carregando...';
 
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const json = await response.json();
 
     if(json.length > 0) {
-        postArea.innerHTML = '';
+        areaPost.innerHTML = ''
 
-        for (let post of json) {
+
+        for(let post of json) {
             let title = document.createElement('h1');
             title.innerHTML = post.title;
 
             let body = document.createElement('p');
             body.innerHTML = post.body;
 
-            postArea.appendChild(title);
-            postArea.appendChild(body);
+            areaPost.appendChild(title);
+            areaPost.appendChild(body);
+
         }
 
     } else {
-        postArea.innerHTML = 'Nenhum post a ser adicionado!'; 
-
-        await fetch('', {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json',
-                
-            }
-        })
+        areaPost.innerHTML = 'Nenhum post foi encontrado!'
     }
 }
 
-async function addNewPost(title, body) {
 
-}
-
-const InserBtn = document.querySelector('#insertBtn').addEventListener('click', async () => {
-    let title = document.querySelector('#titleField').value;
-    let body = document.querySelector('#bodyField').value;
+const insetBtn = document.querySelector('#insertBtn').addEventListener('click', () => {
+    let title = document.querySelector('#titleField').value
+    let body = document.querySelector('#bodyField').value
 
     if(title && body) {
         addNewPost(title, body);
 
     } else {
-        window.alert('Por gentileza preencha todos os campos!')
+        window.alert('Por gentileza preencher todos os itens do formulário!')
     }
-})
+});
+
+async function addNewPost(title, body) {
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            title,
+            body,
+            userId: 2
+        })
+    });
+
+    document.querySelector('#titleField').value = ''
+    document.querySelector('#bodyField').value = ''
+
+}
 
 readPosts();
-
