3aec4aa9f93e68cf2d40a6bc84d2c6fc70932f72
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-02-11 01:25:48 -0300

    commit_project

--- JavaScript/Projetos/jogo_da_velha/src/js/script.js
+++ JavaScript/Projetos/jogo_da_velha/src/js/script.js
@@ -7,42 +7,114 @@ let frame = {
 
 let playerTurn;
 let warning;
-let playig = false;
+let playing = false;
 
-reset();
+
 
 // Eventos
 document.querySelector('.reset').addEventListener('click', reset)
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick)
+});
 
+reset();
 
-// Funções
+// Functions
 function reset() {
     warning = ''
 
     let random = Math.floor(Math.random() * 2)
 
-    random === 0 ? playerTurn = 'x' : playerTurn = 'o';
-//or player = (random === 0) ? 'x' : 'o';
+    //random === 0 ? playerTurn = 'x' : playerTurn = 'o';
+    playerTurn = (random === 0) ? 'x' : 'o';
 
-    for(let i of frame) {
+    for(let i in frame) {
         frame[i] = '';
     }
 
-    playig = true;
+    playing = true;
 
     renderFrame();
     renderInfo();
 }
 
-const renderFrame = () => {
-    for(let i of frame) {
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item')
+    if(playing && frame[item] === '') {
+        frame[item] = playerTurn
+        renderFrame();
+        togglePlayer();
+    }
+    
+}
+
+function renderFrame() {
+    for(let i in frame) {
        let item =  document.querySelector(`div[data-item=${i}]`)
        item.innerHTML = frame[i];
+    }
+
+    checkGame();
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = playerTurn;
+    document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    playerTurn = (playerTurn === 'x') ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerfor('x')) {
+        warning = `O 'X' venceu`
+        playing = false;
+
+    } else if(checkWinnerfor('o')) {
+        warning = `O 'O' venceu`
+        playing = false;
+
+    } else if(isFull()) {
+        warning = 'Deu empate';
+        playing =  false;
 
     }
 }
 
-const renderInfo = () => {
-    document.querySelector('.vez').innerHTML = playerTurn;
-    document.querySelector('.resultado').innerHTML = warning;
+function checkWinnerfor(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ]
+
+    for(let w in pos) {
+        let pArray = pos[w].split(',')
+        let hasWon = pArray.every(option => frame[option] === player)
+        if(hasWon) {
+            return true;
+        }
+    }
+
+    return false
+}
+
+function isFull() {
+    for(let i in frame) {
+        if(frame[i] === '') {
+            return false;
+
+        }
+    }
+
+    return true;
 }
